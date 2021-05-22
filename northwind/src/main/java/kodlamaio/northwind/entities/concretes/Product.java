f0a0c7a81a6abeae339ee29e9e9d44e5c621d75d
kubilaytuncbas <kubilaytuncbas@icloud.com>
2021-05-23 00:54:14 +0300

    7.

--- northwind/src/main/java/kodlamaio/northwind/entities/concretes/Product.java
+++ northwind/src/main/java/kodlamaio/northwind/entities/concretes/Product.java
@@ -1,17 +1,27 @@
 package kodlamaio.northwind.entities.concretes;
 
 import javax.persistence.Column;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
 @Entity
 @Table(name="products")
+@AllArgsConstructor
+@NoArgsConstructor
 public class Product {
 	
+	
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name="product_id")
 	private int id;
 	
@@ -28,57 +38,9 @@ public class Product {
 	private short unitsInStock;
 	
 	@Column(name="quantity_per_unit")
-	private String quantityPerUnitString;
+	private String quantityPerUnit;
 	
-	public Product() {
-		
-	}
 	
-	public Product(int id, int categoryId, String productNameString, double unitPrice, short unitsInStock,
-			String quantityPerUnitString) {
-		super();
-		this.id = id;
-		this.categoryId = categoryId;
-		this.productName = productNameString;
-		this.unitPrice = unitPrice;
-		this.unitsInStock = unitsInStock;
-		this.quantityPerUnitString = quantityPerUnitString;
-	}
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	public int getCategoryId() {
-		return categoryId;
-	}
-	public void setCategoryId(int categoryId) {
-		this.categoryId = categoryId;
-	}
-	public String getProductNameString() {
-		return productName;
-	}
-	public void setProductNameString(String productNameString) {
-		this.productName = productNameString;
-	}
-	public double getUnitPrice() {
-		return unitPrice;
-	}
-	public void setUnitPrice(double unitPrice) {
-		this.unitPrice = unitPrice;
-	}
-	public short getUnitsInStock() {
-		return unitsInStock;
-	}
-	public void setUnitsInStock(short unitsInStock) {
-		this.unitsInStock = unitsInStock;
-	}
-	public String getQuantityPerUnitString() {
-		return quantityPerUnitString;
-	}
-	public void setQuantityPerUnitString(String quantityPerUnitString) {
-		this.quantityPerUnitString = quantityPerUnitString;
-	}
+	
 
 }
